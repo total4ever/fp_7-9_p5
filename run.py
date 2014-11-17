d2b33e141a62f36d5350cb8157768ce0f6bbdf48
Paul <total.name@gmail.com>
2014-11-18 00:23:40 +0200

    Updated run file for events

--- run.py
+++ run.py
@@ -2,10 +2,15 @@ from domain.validators import Validators
 from repository.person_repository import PersonRepository
 from controller.person_ctrl import PersonCtrl
 from ui.console import Console
+from repository.event_repository import EventRepository
+from controller.event_ctrl import EventCtrl
 
 val = Validators()
 person_repo = PersonRepository()
 person_ctrl = PersonCtrl(val, person_repo)
 
-cons = Console(person_ctrl)
+event_repo = EventRepository()
+event_ctrl = EventCtrl(val, event_repo)
+
+cons = Console(person_ctrl, event_ctrl)
 cons.startUI()
