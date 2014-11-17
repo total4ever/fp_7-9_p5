53b5c72cb868bfbc3652761c8486813c01588d71
Paul <total.name@gmail.com>
2014-11-18 00:23:48 +0200

    Updated console for events

--- ui/console.py
+++ ui/console.py
@@ -1,10 +1,13 @@
 class Console():
-    def __init__(self, pctrl):
+    def __init__(self, pctrl, ectrl):
         self.__person_ctrl = pctrl
+        self.__event_ctrl = ectrl
     
     def printMenu(self):
         print("1. Adauga persoana")
         print("2. Afisare persoane")
+        print("3. Adauga eveniment")
+        print("4. Afisare evenimente")
         print("x. Iesire")
         
     def __addPerson(self):
@@ -18,20 +21,47 @@ class Console():
             print("ID persoana deja existent.")
         except ValueError as msg:
             print(msg)
-       
+
+    def __addEvent(self):
+        eventID = int(input("ID: "))
+        date = input("Data: ")
+        time = input("Ora: ")
+        desc = input("Desriere: ")
+
+        try:
+            self.__event_ctrl.addEvent(eventID, date, time, desc)
+        except KeyError:
+            print("ID eveniment deja existent.")
+        except ValueError as msg:
+            print(msg)
+
     def __showAllPersons(self):
         lst = self.__person_ctrl.getAll()
         
         for value in lst:
             p = lst[value]
             print(p.getID(), " -> ", p.getName(), "|", p.getAddr())
+
+    def __showAllEvents(self):
+        lst = self.__event_ctrl.getAll()
+
+        for value in lst:
+            p = lst[value]
+            print(p.getID(), " -> ", p.getDate(), "at", p.getTime(), " | ", p.getDesc())
         
     def startUI(self):
-        while True: 
+        while True:
+            self.printMenu()
+
             cmd = input("Comanda:")
             if cmd == "1":
                 self.__addPerson()
             if cmd == "2":
                 self.__showAllPersons()
+            if cmd == "3":
+                self.__addEvent()
+            if cmd == "4":
+                self.__showAllEvents()
+
             if cmd == "x":
                 break
