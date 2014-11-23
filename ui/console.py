88b5dffbfb474733119a44fe2348aeadbdb944f1
Paul <total.name@gmail.com>
2014-11-23 06:13:49 +0200

    Updated run file

--- ui/console.py
+++ ui/console.py
@@ -1,8 +1,8 @@
 class Console():
-    def __init__(self, pctrl, ectrl):
+    def __init__(self, pctrl, ectrl, actrl):
         self.__person_ctrl = pctrl
         self.__event_ctrl = ectrl
-    
+        self.__attend_ctrl = actrl
     def printMenu(self):
         print("1. Adauga persoana")
         print("2. Afisare persoane")
@@ -12,6 +12,8 @@ class Console():
         print("6. Stergere eveniment")
         print("7. Modificare persoana")
         print("8. Modificare eveniment")
+
+        print("9. Adauga persoana la eveniment")
         print("q. Iesire")
         
     def __addPerson(self):
@@ -87,8 +89,68 @@ class Console():
             self.__event_ctrl.updateEvent(eventID, newdate, newtime, newdesc)
         except KeyError:
             print("ID eveniment deja inexistent.")
-            
+
+    def __attend(self):
+        personID = int(input("ID persoana: "))
+        eventID = int(input("ID eveniment:"))
+
+        try:
+            self.__attend_ctrl.attend(personID, eventID)
+            print("OK")
+        except ValueError as msg:
+            print(msg)
+
+    def __personEvents(self):
+        personID = int(input("ID persoana: "))
+
+        try:
+            data = self.__attend_ctrl.personEvents(personID)
+            for x in data:
+                print(x.getEventDesc())
+        except ValueError as msg:
+            print(msg)
+
+    def __personsWithMostEvents(self):
+        data = self.__attend_ctrl.personsWithMostEvents()
+        for x in data:
+            print(x.getPersonName(), x.getPersonEvents())
+
+    def __eventsWithMostPersons(self):
+        data = self.__attend_ctrl.eventsWithMostPersons()
+
+        for x in data:
+            print(x.getEventDesc(), x.getEventPersons())
+
     def startUI(self):
+        self.__person_ctrl.addPerson(1, "Paul", "Cluj")
+        self.__person_ctrl.addPerson(2, "Paula", "Cluj")
+        self.__person_ctrl.addPerson(3, "Ion", "Cluj")
+        self.__person_ctrl.addPerson(4, "Gheorghe", "Cluj")
+
+        self.__event_ctrl.addEvent(1, "09/11/2014", "18.05", "eveniment 1")
+        self.__event_ctrl.addEvent(2, "09/11/2014", "18.05", "eveniment 2")
+        self.__event_ctrl.addEvent(3, "09/11/2014", "18.05", "eveniment 3")
+        self.__event_ctrl.addEvent(4, "09/11/2014", "18.05", "eveniment 4")
+        self.__event_ctrl.addEvent(5, "09/11/2014", "18.05", "eveniment 5")
+        self.__event_ctrl.addEvent(6, "09/11/2014", "18.05", "eveniment 6")
+
+        self.__attend_ctrl.attend(1, 1)
+        self.__attend_ctrl.attend(1, 2)
+        self.__attend_ctrl.attend(1, 3)
+
+        self.__attend_ctrl.attend(2, 1)
+        self.__attend_ctrl.attend(2, 3)
+
+        self.__attend_ctrl.attend(3, 4)
+        self.__attend_ctrl.attend(3, 5)
+        self.__attend_ctrl.attend(3, 6)
+
+        self.__attend_ctrl.attend(4, 1)
+        self.__attend_ctrl.attend(4, 2)
+        self.__attend_ctrl.attend(4, 3)
+        self.__attend_ctrl.attend(4, 4)
+        self.__attend_ctrl.attend(4, 5)
+
         while True:
             self.printMenu()
 
@@ -109,6 +171,16 @@ class Console():
                 self.__updatePerson()
             if cmd == "8":
                 self.__updateEvent()
+            if cmd == "9":
+                self.__attend()
+
+
+            if cmd == "r1":
+                self.__personEvents()
+            if cmd == "r2":
+                self.__personsWithMostEvents()
+            if cmd == "r3":
+                self.__eventsWithMostPersons()
 
             if cmd == "q":
                 break
