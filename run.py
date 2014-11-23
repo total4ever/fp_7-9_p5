a05d8a61b8f14e5884798b17f593ac89832005ce
Paul <total.name@gmail.com>
2014-11-23 06:14:43 +0200

    Updated run file

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+from controller.attenting_ctrl import AttentingCtrl
 from domain.validators import Validators
+from repository.attenting_repository import AttendingRepository
 from repository.person_repository import PersonRepository
 from controller.person_ctrl import PersonCtrl
 from ui.console import Console
@@ -12,5 +14,8 @@ person_ctrl = PersonCtrl(val, person_repo)
 event_repo = EventRepository()
 event_ctrl = EventCtrl(val, event_repo)
 
-cons = Console(person_ctrl, event_ctrl)
+attend_repo = AttendingRepository()
+attend_ctrl = AttentingCtrl(val, event_repo, person_repo, attend_repo)
+
+cons = Console(person_ctrl, event_ctrl, attend_ctrl)
 cons.startUI()
